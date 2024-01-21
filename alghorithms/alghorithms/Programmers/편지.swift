b694a934fe8c251fd12193186a8140efb0efcf3d
NORIKIM <jinagold.k@gmail.com>
2024-01-21 21:45:21 +0900

    프로그래머스-편지

---
+++ alghorithms/alghorithms/Programmers/편지.swift
@@ -0,0 +1,12 @@
+//
+//  편지.swift
+//  alghorithms
+//
+//  Created by 김지나 on 2024/01/21.
+//
+
+import Foundation
+
+func solution(_ message:String) -> Int {
+    return message.count * 2
+}
--- alghorithms/alghorithms/main.swift
+++ alghorithms/alghorithms/main.swift
@@ -7,5 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
-
+//func solution(_ elements:[Int]) -> Int {
+//    var arr = elements
+//    var result = elements
+//    var num = 1
+//    var plusNum = 0
+//
+//    while true {
+//        for i in 0 ..< elements.count {
+//            if i == elements.count - 1 {
+//                arr.append(contentsOf: elements[0 ... plusNum])
+//                plusNum += 1
+//            }
+//            let sum = arr[i ... i + num].reduce(0,+)
+//            result.append(sum)
+//        }
+//
+//        num += 1
+//
+//        if plusNum == elements.count - 2 {
+//            result.append(elements.reduce(0,+))
+//            return Set(result).count
+//        }
+//    }
+//    return 0
+//}
+//
+//solution([7,9,1,1,4])
