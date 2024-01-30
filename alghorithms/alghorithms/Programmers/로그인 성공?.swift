15d110ca75744a925c0c2f5a9d88ba442b5c8b50
Jina Kim <jinagold.k@gmail.com>
2024-01-30 17:03:57 +0900

    프로그래머스-로그인 성공?

---
+++ alghorithms/alghorithms/Programmers/로그인 성공?.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func solution(_ id_pw:[String], _ db:[[String]]) -> String {
+    let id = id_pw[0]
+    let pw = id_pw[1]
+    
+    for i in 0 ..< db.count {
+        if db[i][0] == id {
+            if db[i][1] == pw {
+                return "login"
+            }
+            else {
+                return "wrong pw"
+            }
+        }
+    }
+    
+    return "fail"
+}
